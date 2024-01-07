1d94f8bc4690b4033947e7e2e57ccc957812c552
avcdsld <avcdsld@gmail.com>
2024-01-07 20:36:11 +0900

    add tool to generate mint codes

---
+++ etc/gen_mint_code.js
@@ -0,0 +1,23 @@
+function generateRandomString(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+const mintCodes = [];
+for (let i = 0; i < 200; i++) {
+    const mintCode = generateRandomString(6);
+    mintCodes.push(mintCode);
+    console.log(mintCode);
+}
+
+// // require ethers.js v6
+// const { solidityPackedKeccak256 } = require('ethers');
+// for (let i = 0; i < 2; i++) {
+//     const mintCodeHash = solidityPackedKeccak256(["string"], [mintCodes[i]]);
+//     console.log(mintCodeHash);
+// }
