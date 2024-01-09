c9fec6571a992370bea8feef5c253f7f2304a30e
avcdsld <avcdsld@gmail.com>
2024-01-09 09:47:45 +0900

    update metadata

--- test/ProofOfDonation.ts
+++ test/ProofOfDonation.ts
@@ -14,8 +14,8 @@ describe("Deploy - Proof of Donation", function () {
     describe("Activate token", function () {
       it("should success", async function () {
         const tokenId = 1;
-        const name = "Noto Earthquake 2024";
-        const description = "Proof of Donation #1 by Proof of X project is in support of the January 2024 Noto Peninsula Earthquake in Japan. This is a memento to prove you have made a donation. All donations will be used to support the victims.";
+        const name = "NAMAZU-E by EXCALIBUR";
+        const description = `Proof of Donation #1 by Proof of X project, a tangible expression of support for the victims of the January 2024 Noto Peninsula Earthquake in Japan. This proof of donation serves as a memento, attesting that you've contributed to the cause. Every donation will be dedicated to aiding those affected.\\n\\nDelving into Japanese folklore, and you'll encounter Namazu, a mythical catfish believed to trigger earthquakes through its tumultuous movements in underground rivers. This age-old tale weaves a narrative that connects natural phenomena with mythical beings, offering an explanation for seismic activities.\\n\\nIn one captivating story, the god Kashima takes center stage, using a colossal stone to subdue Namazu and prevent earthquakes. However, when Kashima lowers his guard, Namazu slips away, setting off tremors. This narrative captures the delicate equilibrium thought to exist between gods and mythical creatures, controlling the forces of nature. \\n\\nThrough the process of minting this NAMAZU-E, which is a NAMAZU art,  on-chain, our project unfolds with the purpose of channeling our thoughts and emotions towards the reconstruction efforts. Additionally, transparent records of donors will be maintained, ensuring a clear and accountable journey towards supporting those in need. The donation will be delivered to Sekijuji.`;
         const image = "https://ara.mypinata.cloud/ipfs/QmNayMJmPnWpb4VeZ6nLT4hh5rMJUAkwboVGCiLVKq3Xkk";
         const artworkCreator = "EXCALIBUR";
         const price = ethers.utils.parseUnits("0.001", "ether");
